73fbb0d20889ce36d622eedc9f29c629e5b32c56
heiwa4126 <heiwa4126@gmail.com>
2024-06-18 11:13:34 +0900

    fix security issues and update modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const e = require("escape-html");
 const app = express();
 
 app.get("/", (req, res) => {
 	const userComment = req.query.comment || "";
 	res.send(`<html><body><h1>User comment:
- ${userComment}
+ ${e(userComment)}
 </h1></body></html>\n`);
 });
 
